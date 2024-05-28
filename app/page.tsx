78263052d93f53f937cc2a872a7f5bfb23f3bbff
Mark Hoffman <7urbo.marko@gmail.com>
2024-05-28 20:36:02 +0100

    Pre-commit test

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,13 @@ import SVG from "react-inlinesvg";
 import { H1 } from "@/components/text";
 
 export default () => {
-  const test = 0;
   return (
     <main className="h-full w-full bg-white">
       {/* Cover and title */}
       <div className="py-28 sm:py-48">
         <SVG src="/logo.svg" className="mx-auto h-40 w-40 sm:w-32" />
-        <H1 className="text-card text-center my-6">This is a subtitle</H1>
+        <H1 className="my-6 text-center text-card">This is a subtitle</H1>
       </div>
     </main>
-  ) 
-}
+  );
+};
